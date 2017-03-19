08d512a7b756c079697ef1fb169a90792bad0ee9
tunnckoCore <mameto_100@mail.bg>
2017-03-19 14:20:55 +0200

    fix(readme): add api docs

--- index.js
+++ index.js
@@ -7,6 +7,51 @@
 
 'use strict'
 
+/**
+ * > A plugin that removes support for emitter methods to be chainable. Basically, by
+ * default [dush][]'s methods (and most of other eventemitters) are chainable
+ * and some user don't like that feature that I need.
+ *
+ * **Example**
+ *
+ * ```js
+ * const dush = require('dush')
+ * const noChaining = require('dush-no-chaining')
+ *
+ * const app = dush()
+ *
+ * // by default they are chainable
+ * app
+ *   .on('foo', () => {})
+ *   .once('bar', () => {})
+ *   .emit('bar', 123)
+ *   .off('foo')
+ *   .emit('bar', 333)
+ *   .emit('foo', 1)
+ *
+ * // but when add this plugin
+ * // they are not chainable
+ * const emitter = dush().use(noChaining())
+ *
+ * const noop = () => {}
+ * const on = emitter.on('foo', noop)
+ * console.log(on) // => undefined
+ *
+ * const off = emitter.off('foo', noop)
+ * console.log(off) // => undefined
+ *
+ * const once = emitter.once('bar', noop)
+ * console.log(once) // => undefined
+ *
+ * const emit = emitter.emit('bar', 123)
+ * console.log(emit) // => undefined
+ * ```
+ *
+ * @name   noChaining
+ * @return {Function} A plugin function that can be passed to [dush][], [minibase][] or [base][]
+ * @api public
+ */
+
 module.exports = function dushNoChaining () {
   return function noChaining (app) {
     var map = {
